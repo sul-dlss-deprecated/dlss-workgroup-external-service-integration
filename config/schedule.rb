65c0619c35fb5635f79c08ab1ca9252e91f6f93e
Chris Beer <cabeer@stanford.edu>
2014-07-22 10:40:40 -0700

    schedule the cronjob to run at 10:30am

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,5 +1,5 @@
 job_type :app_command, '(cd :path && (:task)) :output'
 
-every 1.day, :at => '4:30 am', :roles => [:app] do
+every 1.day, :at => '10:30 am', :roles => [:app] do
   app_command %{ ./bin/roster2csv -c "Rubygems email" | ./bin/add_rubygem_owners -v}
 end
